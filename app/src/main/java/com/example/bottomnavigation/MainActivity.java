a0ee800317df13d4edd1cb5de09eb33fc844bb63
Saimulhoque <saimulhoque8214@gmail.com>
2020-09-11 01:44:55 +0600

    updated

--- app/src/main/java/com/example/bottomnavigation/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.java
@@ -15,27 +15,23 @@ public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
     FrameLayout frameLayout;
-
-
     private HomeFragment homeFragment;
     private ProductFragment productFragment;
     private MessageFragment messageFragment;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        InitializeFragment(new HomeFragment());
+
         homeFragment = new HomeFragment();
         productFragment = new ProductFragment();
         messageFragment = new MessageFragment();
 
-
         bottomNavigationView  = findViewById(R.id.bottmnavigation);
         frameLayout = findViewById(R.id.framelayout);
-
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -57,8 +53,6 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-
-
     }
 
     private void InitializeFragment(Fragment fragment) {
